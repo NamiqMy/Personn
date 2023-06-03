49df19b5686f1467ad47a2e454cfe7325d3d5e01
NamiqMy <namiqmy@code.edu.az>
2023-06-03 11:19:29 +0400

    Create my new Person project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Namiq", "Yaqubov", 35, "80", "182");
+            Console.WriteLine(person.ToString());
+            Console.ReadLine(); 
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Weight { get; set; }
+        public string Height { get; set; }
+
+        public Person(string name, string surname, int age, string weight, string height)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+            this.Weight = weight;
+            this.Height = height;
+
+        }
+        public override string ToString()
+        {
+            return " My name is " + Name + ", surname is " + Surname + ", i am " + Age + " years old " + ", my weight is " + Weight + " kq " + " and my height is " + Height + " sm. ";
+
+        }
+    }
+
+}
